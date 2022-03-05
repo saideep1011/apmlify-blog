acabccef3dfc1c643fdd1af61d8c81b3ced818c2
saideep2311 <saideep2311@gmail.com>
2022-03-05 13:51:02 +0530

    fetch posts from aws

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "bloggapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
